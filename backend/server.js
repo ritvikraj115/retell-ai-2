c931b1548d76c56a1330b3545f60066813aebc2d
Ritvik Raj <143361673+ritvikraj115@users.noreply.github.com>
2025-04-07 14:15:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ app.use(bodyParser.json());
 // Endpoint to create a web call and return the access token
 app.post('/api/create-web-call', async (req, res) => {
   const { agentId, apiKey } = await req.body;
+    console.log(agentId)
   const client = await new Retell({
     apiKey: apiKey, // Replace with your actual API key
   });
